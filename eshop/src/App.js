6096a9d77aaa57c43cf21704e46ba29e1dc21d2d
Ephaste <ephasteniyonizeye@gmail.com>
2024-05-10 16:48:57 +0200

    files

--- eshop/src/App.js
+++ eshop/src/App.js
@@ -1,27 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState} from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+//Pages
+import {Home, Contact} from "./pages"
+
+//Components
+
+import { Header,Footer } from "./components";
 
 function App() {
 
-  const [first, setFirst] = useState("fils")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<BrowserRouter>
+<Header/>
+ <Routes>
+<Route path ="/" element ={<Home/>} />
+<Route path ="/contact" element ={<Contact/>} />
+ </Routes>
+<Footer/>
+
+
+</BrowserRouter>
+</>
+
   );
 }
 
---
+++ eshop/src/pages/admin/Admin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./Admin.module.scss"
+
+const Admin = () => {
+  return (
+    <div>
+      Admin
+    </div>
+  )
+}
+
+export default Admin
